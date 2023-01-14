1db5ac5d3f98f4ee0b8cd5aa1a0f1e1bd0c6233f
GarauOR <luca91garau@gmail.com>
2023-01-14 17:24:07 +0000

    update

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ const productSchema = new mongoose.Schema({
     brand: String,
     price: Number,
     imageUrl: String,
-    description: String
+    description: String,
+    username: String
 });
 const makeupModel = mongoose.model("lipstick", productSchema);
 
@@ -33,15 +34,17 @@ const makeupModel = mongoose.model("lipstick", productSchema);
 //         brand: "maybelline",
 //         price: 14.99,
 //         imageUrl: "https://d3t32hsnjxo7q6.cloudfront.net/i/991799d3e70b8856686979f8ff6dcfe0_ra,w158,h184_pa,w158,h184.png",
-//         description: "Maybelline Face Studio Master Hi-Light Light Boosting bronzer formula has an expert \nbalance of shade + shimmer illuminator for natural glow. Skin goes \nsoft-lit with zero glitz.\n\n\t\tFor Best Results: Brush over all shades in palette and gently sweep over \ncheekbones, brow bones, and temples, or anywhere light naturally touches\n the face.\n\n\t\t\n\t\n\n                    "
-//     });
+//         description: "Maybelline Face Studio Master Hi-Light Light Boosting bronzer formula has an expert \nbalance of shade + shimmer illuminator for natural glow. Skin goes \nsoft-lit with zero glitz.\n\n\t\tFor Best Results: Brush over all shades in palette and gently sweep over \ncheekbones, brow bones, and temples, or anywhere light naturally touches\n the face.\n\n\t\t\n\t\n\n                    ",
+//         username: "luca91garau@gmail.com",
+//      });
 
 //     const contour = new makeupModel ({
 //         name: "contour",
 //         brand: "maybelline",
 //         price: 15.99,
 //         imageUrl: "https://d3t32hsnjxo7q6.cloudfront.net/i/4f731de249cbd4cb819ea7f5f4cfb5c3_ra,w158,h184_pa,w158,h184.png",
-//         description: "Maybelline Facestudio Master Contour Kit is the ultimate on the go all-in-one palette, with contouring brush included.  Define and highlight in a New York minute with this effortless 3-step face contouring kit.  This easy-to-use 3-step face contouring kit features a bronzer, blush and highlighter."
+//         description: "Maybelline Facestudio Master Contour Kit is the ultimate on the go all-in-one palette, with contouring brush included.  Define and highlight in a New York minute with this effortless 3-step face contouring kit.  This easy-to-use 3-step face contouring kit features a bronzer, blush and highlighter.",
+//             username: "luca91garau@gmail.com",
 //     });
 
 //     const blush = new makeupModel ({
@@ -49,13 +52,14 @@ const makeupModel = mongoose.model("lipstick", productSchema);
 //         brand: "maybelline",
 //         price: 14.99,
 //         imageUrl: "https://d3t32hsnjxo7q6.cloudfront.net/i/4621032a92cb428ad640c105b944b39c_ra,w158,h184_pa,w158,h184.png",
-//         description: "Maybelline Face Studio Master Hi-Light Light Boosting blush formula has an expert \nbalance of shade + shimmer illuminator for natural glow. Skin goes \nsoft-lit with zero glitz.\n\n\t\tFor Best Results: Brush over all shades in palette and gently sweep over \ncheekbones, brow bones, and temples, or anywhere light naturally touches\n the face.\n\n\t\t\n\t\n\n                    "
+//         description: "Maybelline Face Studio Master Hi-Light Light Boosting blush formula has an expert \nbalance of shade + shimmer illuminator for natural glow. Skin goes \nsoft-lit with zero glitz.\n\n\t\tFor Best Results: Brush over all shades in palette and gently sweep over \ncheekbones, brow bones, and temples, or anywhere light naturally touches\n the face.\n\n\t\t\n\t\n\n                    ",
+//         username: "jonny.com",
 //     });
 //     bronzer.save();
 //     contour.save();
 //     blush.save();
 // };
-//seedLipstickCollection();
+// seedLipstickCollection();
 
 app.get("/", homeHandler);
 app.get("/product", productsHandler);
@@ -66,8 +70,9 @@ app.put("/product/:id", updateProductHandler);
 
 
 async function productsHandler (req,res) {
-    let productsData = await makeupModel.find({});
+    let productsData = await makeupModel.find({username: req.query.username});
     res.status(200).send(productsData);
+    
 }
 
 async function productByBrandHandler (req,res) {
@@ -95,7 +100,7 @@ async function deleteProductHandler (req,res) {
     const { id } = req.params; 
     const newProduct = await makeupModel.findByIdAndDelete(id);
 
-    let allProds = await makeupModel.find({});
+    let allProds = await makeupModel.find({username:req.query.username});
     res.status(200).send(allProds);
 }
 
@@ -104,7 +109,7 @@ async function updateProductHandler (req,res) {
     const { name, brand, price, imageUrl, description } = req.body;
     const newProduct = await makeupModel.findByIdAndUpdate(id, { name, brand, price, imageUrl, description });
 
-    let allProds = await makeupModel.find({});
+    let allProds = await makeupModel.find({username:req.query.username});
     res.status(200).send(allProds);
 }
 
